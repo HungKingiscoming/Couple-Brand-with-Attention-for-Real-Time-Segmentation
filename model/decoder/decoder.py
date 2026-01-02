dd9ecb0b767269c9eeadc678fab10159023d070b
Giang Tuấn Hùng <123647321+HungKingiscoming@users.noreply.github.com>
2026-01-03 00:37:29 +0700

    Update decoder.py

--- model/decoder/decoder.py
+++ model/decoder/decoder.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
-from typing import List, Tuple, Optional
+from typing import List, Tuple, Optional, Dict
 
 # Lưu code vào file: decoder.py
 
