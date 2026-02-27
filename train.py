a352b70f560baa3f076d173e101a99a225257e19
Giang Tuấn Hùng <123647321+HungKingiscoming@users.noreply.github.com>
2026-02-27 16:51:04 +0700

    Update train.py

--- train.py
+++ train.py
@@ -651,13 +651,10 @@ class Trainer:
                         align_corners=False
                     )
                     aux_ce_loss = self.ce(aux_logits, masks)
-                    if self.dice_weight > 0:
-                        aux_dice_loss = self.dice(aux_logits, masks)
-                    else:
-                        aux_dice_loss = torch.tensor(0.0, device=logits.device)
-                    aux_total = self.ce_weight * aux_ce_loss + self.dice_weight * aux_dice_loss
+
                     aux_weight = self.args.aux_weight * (1 - epoch / self.args.epochs) ** 0.9
-                    loss = loss + aux_weight * aux_total
+            
+                    loss = loss + aux_weight * aux_ce_loss
     
                 loss = loss / self.args.accumulation_steps
     
