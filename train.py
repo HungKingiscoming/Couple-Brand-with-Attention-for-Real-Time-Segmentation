36e03e745e03db314d3614393377845b6b279798
Giang Tuấn Hùng <123647321+HungKingiscoming@users.noreply.github.com>
2026-02-28 12:59:28 +0700

    Update train.py

--- train.py
+++ train.py
@@ -517,7 +517,7 @@ class ModelConfig:
                 "ppm_channels": 128,
                 "num_blocks_per_stage": [4, 4, [5, 4], [5, 4], [2, 2]],
                 "dwsa_stages": ['stage4','stage5', 'stage6'],
-                "dwsa_num_heads": 2,
+                "dwsa_num_heads": 4,
                 "dwsa_reduction": 4,
                 "dwsa_qk_sharing": True,
                 "dwsa_groups": 4,
