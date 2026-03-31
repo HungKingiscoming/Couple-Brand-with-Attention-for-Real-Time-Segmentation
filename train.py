780682eafc50829e0b09b346a8ad69a77bc3af6e
Giang Tuấn Hùng <123647321+HungKingiscoming@users.noreply.github.com>
2026-03-31 20:11:04 +0700

    Fix GradScaler initialization with use_amp argument

--- train.py
+++ train.py
@@ -554,7 +554,7 @@ class Trainer:
         self.base_loss_cfg = loss_cfg
         self.loss_phase = 'full'
         
-        self.scaler = GradScaler(enabled=args.)
+        self.scaler = GradScaler(enabled=args.use_amp)
         
         self.save_dir = Path(args.save_dir)
         self.save_dir.mkdir(parents=True, exist_ok=True)
