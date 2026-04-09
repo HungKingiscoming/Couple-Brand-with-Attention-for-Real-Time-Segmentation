059e73574e293b2c1b59ab5ba52cba7e109d0aef
Giang Tuấn Hùng <123647321+HungKingiscoming@users.noreply.github.com>
2026-04-09 11:53:17 +0700

    Update custom.py

--- data/custom.py
+++ data/custom.py
@@ -8,7 +8,7 @@ from PIL import Image
 from typing import Callable, Optional, Tuple, List, Dict
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
-from albumentations.augmentations import functional as AF
+
 import cv2
 
 
